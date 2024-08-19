b32d89d2ded97ec2703faa2133c08096653d0e6f
pankaj suthar <pankajss0070@gmail.com>
2024-08-19 21:12:29 +0530

    log to see changes in email

--- backend/utils/resend.js
+++ backend/utils/resend.js
@@ -4,7 +4,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 async function sendPasswordResetEmail(email, name, resetTokenLink) {
   try {
-    await resend.emails.send({
+    const result = await resend.emails.send({
       from: "onboarding@resend.dev",
       to: email,
       subject: "Password Reset",
@@ -21,6 +21,7 @@ async function sendPasswordResetEmail(email, name, resetTokenLink) {
 </div>
 `,
     });
+    console.log(result);
     return {
       success: true,
       message: "Password Reset link send to email successfully",
